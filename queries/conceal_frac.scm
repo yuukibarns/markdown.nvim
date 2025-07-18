5971a8f2cb6ffb68da131d71fe5e803bad3b9c9b
yuukibarns <yuukibarns@gmail.com>
2025-07-18 13:21:39 +0800

    frac

--- queries/conceal_frac.scm
+++ queries/conceal_frac.scm
@@ -4,7 +4,7 @@
     (#eq? @cmd "\\frac"))
   (#has-ancestor? @cmd math_environment inline_formula displayed_equation)
   (#set! priority 101)
-  (#set! conceal "("))
+  (#set! conceal "⟮"))
 
 (generic_command
   command: (command_name) @_cmd
@@ -21,7 +21,7 @@
     "}" @close1)
   arg: (curly_group)
   (#has-ancestor? @close1 math_environment inline_formula displayed_equation)
-  (#set! conceal "/"))
+  (#set! conceal "⧸"))
 
 (generic_command
   command: (command_name) @_cmd
@@ -30,7 +30,7 @@
   arg: (curly_group
     "{" @open2)
   (#has-ancestor? @open2 math_environment inline_formula displayed_equation)
-  (#set! conceal "/"))
+  (#set! conceal "⧸"))
 
 (generic_command
   command: (command_name) @_cmd
@@ -39,15 +39,14 @@
   arg: (curly_group
     "}" @close2)
   (#has-ancestor? @close2 math_environment inline_formula displayed_equation)
-  (#set! conceal ")"))
+  (#set! conceal "⟯"))
 
-; Conceal \dfrac command and brackets to show as numerator/denominator
 (generic_command
   command: ((command_name) @cmd
     (#eq? @cmd "\\dfrac"))
   (#has-ancestor? @cmd math_environment inline_formula displayed_equation)
   (#set! priority 101)
-  (#set! conceal "("))
+  (#set! conceal "⟮"))
 
 (generic_command
   command: (command_name) @_cmd
@@ -64,7 +63,7 @@
     "}" @close1)
   arg: (curly_group)
   (#has-ancestor? @close1 math_environment inline_formula displayed_equation)
-  (#set! conceal "/"))
+  (#set! conceal "⧸"))
 
 (generic_command
   command: (command_name) @_cmd
@@ -73,7 +72,7 @@
   arg: (curly_group
     "{" @open2)
   (#has-ancestor? @open2 math_environment inline_formula displayed_equation)
-  (#set! conceal "/"))
+  (#set! conceal "⧸"))
 
 (generic_command
   command: (command_name) @_cmd
@@ -82,4 +81,4 @@
   arg: (curly_group
     "}" @close2)
   (#has-ancestor? @close2 math_environment inline_formula displayed_equation)
-  (#set! conceal ")"))
+  (#set! conceal "⟯"))
